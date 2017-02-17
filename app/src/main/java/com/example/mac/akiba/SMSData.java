0df6c4e9515d95a75de7f59ed32bc68b786c1e8a
Le-Yo <le-yo@le-yo.com>
2017-02-17 13:53:55 +0300

    reading SMSnow working

---
+++ app/src/main/java/com/example/mac/akiba/SMSData.java
@@ -0,0 +1,28 @@
+package com.example.mac.akiba;
+
+/**
+ * Created by Leyo on 17/02/2017.
+ */
+
+public class SMSData {
+    // Number from witch the sms was send
+    private String number;
+    // SMS text body
+    private String body;
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+}
---
+++ app/src/main/java/com/example/mac/akiba/SMSListAdapter.java
@@ -0,0 +1,52 @@
+package com.example.mac.akiba;
+
+import android.app.ListActivity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+import com.example.mac.akiba.SMSData;
+
+/**
+ * Created by Leyo on 17/02/2017.
+ */
+
+public class SMSListAdapter extends ArrayAdapter<SMSData> {
+    // List context
+    private final Context context;
+    // List values
+    private final List<SMSData> smsList;
+
+
+    public SMSListAdapter(Context context,  List<SMSData> smsList) {
+        super(context, R.layout.read_sms,smsList);
+        this.context = context;
+        this.smsList = smsList;
+    }
+
+//    public ListAdapter(Context context, List<SMSData> smsList) {
+//
+//        super(context, R.layout.read_sms, smsList);
+//        this.context = context;
+//        this.smsList = smsList;
+//    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View rowView = inflater.inflate(R.layout.read_sms, parent, false);
+        //if(smsList.get(position).getNumber() == 'MSHWARI'){
+            TextView senderNumber = (TextView) rowView.findViewById(R.id.smsNumberText);
+            senderNumber.setText(smsList.get(position).getNumber());
+
+        //}
+
+        return rowView;
+    }
+
+}
